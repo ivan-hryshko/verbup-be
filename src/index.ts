4a781e8f5199a84a8a7259139e00e0155954ff4d
ivan-hryshko <giver313@gmail.com>
2025-05-20 20:29:31 +0300

    fix: listen app

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import ENVS from './config/envs'
 const startServer = async () => {
   try {
     app.listen(ENVS.APP_PORT, () => {
-      console.log(`Movies app listening at http://localhost:${ENVS.APP_PORT}`)
+      console.log(`VerbUp app listening at http://localhost:${ENVS.APP_PORT}`)
     })
   } catch (error) {
     console.error('Unable to connect to the database:', error)
