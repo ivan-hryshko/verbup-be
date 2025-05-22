add21a3dd2b3ee4efb9a9b0c25ea3e97bad7463e
Eugene-Khonich <ehonich@gmail.com>
2025-05-22 23:25:35 +0300

    Вніс невеликі правки

--- src/modules/users/users.route.ts
+++ src/modules/users/users.route.ts
@@ -4,10 +4,10 @@ import { ctrlWrapper } from '../../utils/ctrlWrapper'
 
 const router = Router()
 
-router.post('/users', ctrlWrapper(UsersController.create))
-router.get('/users', ctrlWrapper(UsersController.getAll))
-router.get('/users/:id')
-router.patch('/users/:id')
-router.delete('users/:id')
+router.post('/', ctrlWrapper(UsersController.create))
+router.get('/', ctrlWrapper(UsersController.getAll))
+router.get('/:id', ctrlWrapper(UsersController.getById))
+router.patch('/:id', ctrlWrapper(UsersController.update))
+router.delete('/:id', ctrlWrapper(UsersController.delete))
 
 export default router
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,10 @@
 import appDataSource from '../../config/app-data-source'
 import { UserEntity } from './users.entity'
 
+const userRepository = appDataSource.getRepository(UserEntity)
+
 export class UsersService {
   static async create(data: Partial<UserEntity>): Promise<UserEntity> {
-    const userRepository = appDataSource.getRepository(UserEntity)
     const existingUser = await userRepository.findOne({
       where: { email: data.email },
     })
@@ -15,16 +16,34 @@ export class UsersService {
   }
 
   static async getAll(): Promise<UserEntity[]> {
-    const userRepository = appDataSource.getRepository(UserEntity)
     return await userRepository.find()
   }
 
   static async getById(id: number): Promise<UserEntity | null> {
-    const userRepository = appDataSource.getRepository(UserEntity)
     const user = userRepository.findOne({ where: { id } })
     if (!user) {
       throw new Error('User not found')
     }
     return user
+  }
+
+  static async update(
+    id: number,
+    data: Partial<UserEntity>
+  ): Promise<UserEntity> {
+    const user = await userRepository.findOne({ where: { id } })
+    if (!user) {
+      throw new Error('User not found')
+    }
+    Object.assign(user, data)
+    return await userRepository.save(user)
+  }
+
+  static async delete(id: number): Promise<void> {
+    const user = await userRepository.findOne({ where: { id } })
+    if (!user) {
+      throw new Error('User not found')
+    }
+    await userRepository.remove(user)
   }
 }
