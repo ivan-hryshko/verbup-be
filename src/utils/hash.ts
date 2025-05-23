fea099f7dc1aa7cfdf312fed061cbdd8e2ee1a7a
Eugene-Khonich <ehonich@gmail.com>
2025-05-23 22:32:50 +0300

    Додав хешування паролю і створив верифікацію при логіні

---
+++ src/utils/hash.ts
@@ -0,0 +1,23 @@
+import crypto from 'crypto'
+import { promisify } from 'util'
+
+const scrypt = promisify(crypto.scrypt)
+
+export async function hashPassword(password: string): Promise<string> {
+  const salt = crypto.randomBytes(16).toString('hex')
+  const derivedKey = (await scrypt(password, salt, 64)) as Buffer
+  return `${salt}:${derivedKey.toString('hex')}`
+}
+
+export async function verifyPassword(
+  password: string,
+  storedHash: string
+): Promise<boolean> {
+  const [salt, key] = storedHash.split(':')
+  const derivedKey = (await scrypt(password, salt, 64)) as Buffer
+  return derivedKey.toString('hex') === key
+}
+
+// const hashed = await hashPassword('mysecret');
+// const isMatch = await verifyPassword('mysecret', hashed);
+// console.log('Password match:', isMatch); // true
