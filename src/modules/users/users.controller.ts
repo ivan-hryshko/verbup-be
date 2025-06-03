37a224a1aeb29e31590aed147c6c7709e79f58f9
Eugene-Khonich <ehonich@gmail.com>
2025-06-03 20:34:01 +0300

    фікс

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -4,29 +4,33 @@ import { UsersService } from './users.service'
 export class UsersController {
   private readonly service = new UsersService()
 
-  async create(req: Request, res: Response, next: NextFunction): Promise<any> {
+  create = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
     const user = await this.service.create(req.body)
     res.status(201).json(user)
   }
 
-  async getAll(req: Request, res: Response) {
+  getAll = async (req: Request, res: Response) => {
     const users = await this.service.getAll()
     res.status(200).json(users)
   }
 
-  async getById(req: Request, res: Response) {
+  getById = async (req: Request, res: Response) => {
     const { id } = req.params
     const user = await this.service.getById(Number(id))
     res.status(200).json(user)
   }
 
-  async update(req: Request, res: Response) {
+  update = async (req: Request, res: Response) => {
     const { id } = req.params
     const user = await this.service.update(Number(id), req.body)
     res.status(200).json(user)
   }
 
-  async delete(req: Request, res: Response) {
+  delete = async (req: Request, res: Response) => {
     const { id } = req.params
     await this.service.delete(Number(id))
     res.status(204).send()
