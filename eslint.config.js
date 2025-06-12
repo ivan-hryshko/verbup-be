767534572b15fea3fe6de1e9664a48c84e252d43
ivan-hryshko <giver313@gmail.com>
2025-06-12 11:17:17 +0300

    feat: added aitbnb eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,75 @@
-// eslint.config.js
-const js = require('@eslint/js')
-const tseslint = require('typescript-eslint')
-const prettierPlugin = require('eslint-plugin-prettier')
-const prettierConfig = require('eslint-config-prettier')
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+const importPlugin = require('eslint-plugin-import');
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 module.exports = [
   {
     ignores: ['dist', 'node_modules', 'eslint.config.js', 'jest.config.js'],
   },
+
   js.configs.recommended,
   ...tseslint.configs.recommended,
+
   {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+      },
+    },
     plugins: {
+      import: importPlugin,
       prettier: prettierPlugin,
     },
     rules: {
+      // Airbnb style rules
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/namespace': 'error',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.test.ts',
+            '**/*.spec.ts',
+            '**/*.config.ts',
+            '**/jest.setup.ts',
+          ],
+        },
+      ],
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+
+      // Node best practices
+      'no-console': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+
+      // Prettier integration
       'prettier/prettier': 'error',
     },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          // always try to resolve types under `<root>@types` directory even it doesn't contain source code
+          alwaysTryTypes: true,
+          project: './tsconfig.json', // path to your tsconfig file
+        },
+      },
+    },
   },
+
+  // Turn off conflicting rules with Prettier
   prettierConfig,
-]
+];
